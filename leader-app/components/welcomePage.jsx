4c153a0e698c8d7ed168ccc42909678b147b9596
david8219501 <8487353@gmail.com>
2024-09-12 10:38:30 +0300

    Small changes in the design of the main page

--- leader-app/components/welcomePage.jsx
+++ leader-app/components/welcomePage.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 import Data from '../data.json';
 
 // פונקציה לקביעת הודעת הברכה
@@ -32,13 +33,16 @@ export default function WelcomePage() {
         <Text style={styles.greetingPart2}>{greetingText[1]}</Text>
       </Text>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button('#cddcee')} onPress={() => console.log('Button Pressed!')}>
+        <TouchableOpacity style={styles.button('#9bc0ee')} onPress={() => console.log('Button Pressed!')}>
+          <MaterialIcons name="calendar-today" size={30} color="#2e6eb7" />
           <Text style={styles.buttonText}>מערכת שעות</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button('#e3f3f4')} onPress={() => console.log('Button Pressed!')}>
+        <TouchableOpacity style={styles.button('#e1b0e3')} onPress={() => console.log('Button Pressed!')}>
+          <MaterialIcons name="group" size={30} color="#2e6eb7" />
           <Text style={styles.buttonText}>ניהול עובדים</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button('#e5d2e6')} onPress={() => console.log('Button Pressed!')}>
+        <TouchableOpacity style={styles.button('#b6ecf0')} onPress={() => console.log('Button Pressed!')}>
+          <MaterialIcons name="settings" size={30} color="#42658d" />
           <Text style={styles.buttonText}>הגדרות</Text>
         </TouchableOpacity>
       </View>
@@ -85,7 +89,7 @@ const styles = StyleSheet.create({
   }),
   buttonText: {
     fontSize: 20,
-    color: '#2e6eb7',
+    color: 'gray',
     fontFamily: 'ComicSansMS',
     fontWeight: 'bold',
     textAlign: 'center',
